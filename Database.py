92c1c11d473cf5894608b660d46b7da0a0c0b415
Nehir Akbulut <nickohre@outlook.com>
2022-09-21 12:29:49 +0200

    feat: add azure cosmosdb implementation

--- Database.py
+++ Database.py
@@ -1,6 +1,8 @@
 import os
+import azure.cosmos.cosmos_client as cosmos_client
 from utils.config import config
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+from azure.storage.blob import BlobServiceClient, ContainerClient, __version__
+from azure.cosmos.partition_key import PartitionKey
 
 
 class Database:
@@ -10,10 +12,20 @@ class Database:
         self.blob_client = None
 
         try:
-            print("Azure Blob Storage v" + __version__)
+            print("Azure Blob Storage: v" + __version__)
 
         except Exception as ex:
-            print('Exception: '+ex)
+            print('Exception: '+str(ex))
+
+        self.cosmosclient = cosmos_client.CosmosClient(config["azure_cosmos"]["account_url"], config["azure_cosmos"]["account_key"])
+        self.cosmosdb = self.cosmosclient.create_database_if_not_exists(config["azure_cosmos"]["db_name"])
+        self.cosmoscontainer = self.cosmosdb.create_container_if_not_exists(config["azure_cosmos"]["con_name"], PartitionKey(path=config["azure_cosmos"]["part_key"], kind='Hash'))
+
+        try:
+            print("Azure Cosmos DB: "+ str(self.cosmosclient))
+        
+        except Exception as ex:
+            print('Exception: '+str(ex))
 
     def set_container(self, name):
         blob_client = ContainerClient.from_connection_string(config['azure_storage_connection_string'], name)
@@ -24,5 +36,30 @@ class Database:
         print("Selected container: " + self.blob_client.container_name)
 
     def upload_blob(self, blobname, file):
-        with open(file, 'rb') as data:
-            self.blob_client.upload_blob(blobname, data)
+        try:
+            with open(file, 'rb') as data:
+                blob = self.blob_client.upload_blob(blobname, data)
+            self.upload_blob_reference(blobname, blob.url)
+
+        except Exception as ex:
+            print('Load fail: '+str(ex))
+    
+    def upload_blob_reference(self, id, reference):
+        try:
+            item = self.cosmoscontainer.read_item(id, id.split("+")[0])
+            new_item = item
+            new_item.update({"frame": reference})
+            self.cosmoscontainer.replace_item(item, new_item)
+
+        except:
+            item = self.cosmoscontainer.create_item(
+                {
+                    "id": id,
+                    "cam": id.split("+")[0],
+                    "datetime": None, 
+                    "sensor1": None,
+                    "sensor2": None,
+                    "sensor3": None,
+                    "frame": reference
+                }
+            )
--- Stream.py
+++ Stream.py
@@ -18,5 +18,5 @@ class Stream:
             self.simulation_url = video_info.get('url')
 
     def get_frames(self):
-        ffmpeg_command = 'ffmpeg -i "'+self.simulation_url+'" -vf fps='+str(self.fps)+'/60 -loglevel quiet -hide_banner -strftime 1 '+self.name+'/'+self.name+'+%d-%m-%Y_%H-%M-%S.jpg'
+        ffmpeg_command = 'ffmpeg -i "'+self.simulation_url+'" -vf fps='+str(self.fps)+'/60 -loglevel quiet -hide_banner -strftime 1 '+self.name+'/'+self.name+'+%d-%m-%Y_%H-%M-00.jpg'
         os.system(ffmpeg_command)
